0761211a52fdcdfa57781a9e25b1e872552d8f6d
Subhadip Das <subhadipdas911@gmail.com>
2024-02-19 23:33:58 +0530

    My First React Project

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// const Experience = function(){
+//   return (
+//     <div>
+//       <h2>My Experience</h2>
+//       <ul>
+//         <li>Accenture</li>
+//         <li>Teaching Assistant</li>
+//       </ul>
+//     </div>
+//   )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+  // <div>
+  //   <h1>Hello React World</h1>
+  //   <Experience/>
+    
+  // </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+reportWebVitals();
