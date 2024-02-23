a1bc2043084017a5a0ba66314647cfa3650ea5f6
Subhadip Das <subhadipdas911@gmail.com>
2024-02-23 18:49:14 +0530

    Added new Todo and also cleared existing todo from I/P Box

--- todo-app/src/Components/TodoList.js
+++ todo-app/src/Components/TodoList.js
@@ -16,23 +16,39 @@ let TodoList = function(){
         {title:"Item3"},
         {title:"Item4"},
         {title:"Item5"}
-    ])
+    ]);
+    
+    let [newTodo,setNewTodo] = useState("");
+
+    let handleNewTodoChange = function(e){
+        setNewTodo(e.target.value);
+    }
+
+    let handleTodoSubmit = function(e){ // Upon Clicking on the AddNewTodo Button we will add the new todo the todo list
+        console.log(newTodo); // User has currently typed the word
+        // setTodos(todo.push({title:newTodo}));
+        setTodos([...todo,{title:newTodo}]);
+        setNewTodo("") // The value just entered inside the I/P will be initialized to "" inside SV.
+    }
+
     return(
         <div className="parent-wrapper">
             {/* <h2>Todo Application</h2> */}
         <div className="todo-wrapper">
-            <div className='top-half'>
-                <input placeholder='Please enter a todo'></input>
+            <div className='todo-form'>
+                <input type='text' value={newTodo} placeholder='Please enter a todo' onChange={handleNewTodoChange}></input>
                 <select>
                     <option>Complete</option>
                     <option>Pending</option>
                 </select>
-                <button>Add Todo</button>
+                <button onClick={handleTodoSubmit}>Add Todo</button>
             </div>
             <div className="todo-list-wrapper">
-                {todo.map(function(ele){
-                    return(<p>{ele.title}</p>);
-                })}
+                {
+                todo.map(function(ele){
+                    return(<p>{ele.title}</p>)
+                })
+                }
             </div>
         </div>
         </div>
