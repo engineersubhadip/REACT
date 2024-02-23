f101b6e692de4466e0b9a2bd2f45dc44b4609f63
Subhadip Das <subhadipdas911@gmail.com>
2024-02-23 20:57:18 +0530

    Will be able to strike-through existing and new todos

--- todo-app/src/Components/TodoList.js
+++ todo-app/src/Components/TodoList.js
@@ -1,58 +1,90 @@
-import './../Styles/todo-styling.css'
+import "./../Styles/todo-styling.css";
 import { useState } from "react";
 
-let TodoList = function(){
-    // let data = ["Item1","Item2","Item3","Item4"];
-    // let data = [
-    //     {title:"Item1"},
-    //     {title:"Item2"},
-    //     {title:"Item3"},
-    //     {title:"Item4"}
-    // ]
+let TodoList = function () {
+  // let data = ["Item1","Item2","Item3","Item4"];
+  // let data = [
+  //     {title:"Item1"},
+  //     {title:"Item2"},
+  //     {title:"Item3"},
+  //     {title:"Item4"}
+  // ]
 
-    let [todo,setTodos] = useState([
-        {title:"Item1"},
-        {title:"Item2"},
-        {title:"Item3"},
-        {title:"Item4"},
-        {title:"Item5"}
-    ]);
-    
-    let [newTodo,setNewTodo] = useState("");
+  let [todo, setTodos] = useState([
+    { title: "Item1", isCompleted: true },
+    { title: "Item2", isCompleted: false },
+    { title: "Item3", isCompleted: true },
+    { title: "Item4", isCompleted: false },
+    { title: "Item5", isCompleted: true },
+  ]);
 
-    let handleNewTodoChange = function(e){
-        setNewTodo(e.target.value);
-    }
+  let [newTodo, setNewTodo] = useState("");
 
-    let handleTodoSubmit = function(e){ // Upon Clicking on the AddNewTodo Button we will add the new todo the todo list
-        console.log(newTodo); // User has currently typed the word
-        // setTodos(todo.push({title:newTodo}));
-        setTodos([...todo,{title:newTodo}]);
-        setNewTodo("") // The value just entered inside the I/P will be initialized to "" inside SV.
-    }
+  let handleNewTodoChange = function (e) {
+    setNewTodo(e.target.value);
+  };
 
-    return(
-        <div className="parent-wrapper">
-            {/* <h2>Todo Application</h2> */}
-        <div className="todo-wrapper">
-            <div className='todo-form'>
-                <input type='text' value={newTodo} placeholder='Please enter a todo' onChange={handleNewTodoChange}></input>
-                <select>
-                    <option>Complete</option>
-                    <option>Pending</option>
-                </select>
-                <button onClick={handleTodoSubmit}>Add Todo</button>
-            </div>
-            <div className="todo-list-wrapper">
-                {
-                todo.map(function(ele){
-                    return(<p>{ele.title}</p>)
-                })
-                }
-            </div>
+  let handleTodoSubmit = function (e) {
+    // Upon Clicking on the AddNewTodo Button we will add the new todo the todo list
+    e.preventDefault(); // To stop the page from reloading
+    console.log(newTodo); // User has currently typed the word
+    let newTodoList = [...todo, { title: newTodo }];
+    setTodos(newTodoList);
+    setNewTodo(""); // The value just entered inside the I/P will be initialized to "" inside SV.
+  };
+
+  let handleCompletion = function(e){
+    let currentIndexClicked = e.target.dataset.id;
+    let currentIsCompletedStatus = e.target.checked;
+
+    // Lets create a copy of the todoList:-
+    let newTodoList = [...todo];
+
+    // Where is the element present in this todoList and make a copy of that object:-
+    let currentElement = {...newTodoList[currentIndexClicked]};
+    currentElement.isCompleted = currentIsCompletedStatus; // We have updated the copied element
+
+    // Now we will replace:-
+
+    newTodoList.splice(currentIndexClicked,1,currentElement);
+
+    // Update the State Variable:-
+
+    setTodos(newTodoList);
+  }
+
+  return (
+    <div className="parent-wrapper">
+      <h3>Todo Application</h3>
+      <div className="todo-wrapper">
+        <div className="todo-form">
+          <form>
+            <input
+              type="text"
+              value={newTodo}
+              placeholder="Please enter a todo"
+              onChange={handleNewTodoChange}
+            ></input>
+            <select>
+              <option>Complete</option>
+              <option>Pending</option>
+            </select>
+            <button onClick={handleTodoSubmit}>Add Todo</button>
+          </form>
         </div>
+        <div className="todo-list-wrapper">
+          {todo.map(function (ele,idx) {
+            return (
+              <p>
+                <input type="checkbox" checked={ele.isCompleted} data-id={idx} onChange={handleCompletion} />
+                <span className={ele.isCompleted ? "task-complete":""}>{ele.title}</span>
+              </p>
+            );
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
