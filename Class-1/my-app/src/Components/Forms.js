10b9c1877a544f515cf1e6e1c6a30d00648bcdc0
Subhadip Das <subhadipdas911@gmail.com>
2024-03-23 19:49:33 +0530

    Implemented the check that if the user is typing @, the user cannot type that

--- Class-1/my-app/src/Components/Forms.js
+++ Class-1/my-app/src/Components/Forms.js
@@ -5,19 +5,24 @@ let Forms = function(){
     let [firstName,setFirstName] = useState("");
     
     let updateFirstName = function(e){
+        let currChar = e.target.value[e.target.value.length-1];
+        if (currChar === "@"){
+            console.log("Special Character @ is not allowed");
+            return;
+        }else{
+            setFirstName(e.target.value);
+        }
         // if(e.target.value !== "@" ){ // By doing this check we are ensuring that when the user is typing "@" we are not showing that in the UI. Because we have control over the UI.
         //     console.log("Getting triggered")
         //     setFirstName(e.target.value);
         // }
-        if (e.target.value.length > 5){
-            console.log(firstName);
-            console.log("Not valid");
-            return;
-        }
-        setFirstName(e.target.value);
-        console.log(firstName);
-        // console.log(e.target.value);
+        // if (e.target.value.length > 5){
+        //     console.log(firstName);
+        //     console.log("Not valid");
+        //     return;
+        // }
         // setFirstName(e.target.value);
+        // console.log(firstName);
     }
 
     return (
