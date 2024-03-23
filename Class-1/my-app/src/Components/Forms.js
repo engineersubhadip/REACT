773795e2c8996ecd5b6de9be3d91cfc9e3681269
Subhadip Das <subhadipdas911@gmail.com>
2024-03-23 19:55:00 +0530

    Provided with the relevant comments

--- Class-1/my-app/src/Components/Forms.js
+++ Class-1/my-app/src/Components/Forms.js
@@ -5,6 +5,8 @@ let Forms = function(){
     let [firstName,setFirstName] = useState("");
     
     let updateFirstName = function(e){
+        /*
+        // ! If the user types in @
         let currChar = e.target.value[e.target.value.length-1];
         if (currChar === "@"){
             console.log("Special Character @ is not allowed");
@@ -12,15 +14,13 @@ let Forms = function(){
         }else{
             setFirstName(e.target.value);
         }
-        // if(e.target.value !== "@" ){ // By doing this check we are ensuring that when the user is typing "@" we are not showing that in the UI. Because we have control over the UI.
-        //     console.log("Getting triggered")
-        //     setFirstName(e.target.value);
-        // }
-        // if (e.target.value.length > 5){
-        //     console.log(firstName);
-        //     console.log("Not valid");
-        //     return;
-        // }
+        */
+
+        if (e.target.value.length > 5){
+            console.log(firstName);
+            console.log("Not valid");
+            return;
+        }
         // setFirstName(e.target.value);
         // console.log(firstName);
     }
