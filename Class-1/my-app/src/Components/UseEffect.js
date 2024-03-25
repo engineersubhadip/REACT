da050aae90c9438c2b467b9cd87cd95c8cf59822
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 11:51:25 +0530

    cb3 is run when state count and text change

--- Class-1/my-app/src/Components/UseEffect.js
+++ Class-1/my-app/src/Components/UseEffect.js
@@ -2,20 +2,25 @@ import { useEffect, useState } from "react";
 
 let UseEffect = function(){
     let [count,setCount] = useState(0);
-    
+    let [text,setText] = useState("");
+
     let valInc = function(){
         setCount(count+1);
+    } 
+
+    let handleChange = function(e){
+        setText(e.target.value);
     }
 
-    useEffect(function cb1(){
-        console.log("This is to be printed in the Mounting Phase");
-    },[]);
+    // useEffect(function cb1(){
+    //     console.log("This is to be printed in the Mounting Phase");
+    // },[]);
 
     // * cb1 -> will be executed when the component renders for the first time in the UI.
 
-    useEffect(function cb2(){
-        console.log("This is to be printed in the Mounting and Update Phase");
-    },[count]);
+    // useEffect(function cb2(){
+    //     console.log("This is to be printed in the Mounting and Update Phase");
+    // },[count]);
 
     // * cb2 -> will be executed when the component renders for the first time in the UI and when STATE Changes.
 
@@ -29,6 +34,8 @@ let UseEffect = function(){
     return(
         <div>The value of count is : {count}
         <button onClick={valInc}>Increment</button>
+        <input type="text" onChange={handleChange} value={text}></input>
+        <h2>{text}</h2>
         </div>
         
     )
