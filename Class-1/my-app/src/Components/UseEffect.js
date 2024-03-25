0951e7a642c3fe1396c003ccc0f034e3f29f4e2d
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 11:44:18 +0530

    What happens when no dependency array is passed

--- Class-1/my-app/src/Components/UseEffect.js
+++ Class-1/my-app/src/Components/UseEffect.js
@@ -19,6 +19,13 @@ let UseEffect = function(){
 
     // * cb2 -> will be executed when the component renders for the first time in the UI and when STATE Changes.
 
+
+    useEffect(function cb3(){
+        console.log("This will also run in the Mounting and Updating Phase");
+    })
+
+    // * cb3 -> will be executed in the Mounting and Updating Phase
+
     return(
         <div>The value of count is : {count}
         <button onClick={valInc}>Increment</button>
