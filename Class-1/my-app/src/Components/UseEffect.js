6ba2b33ddc8bd5fe0a252853039bffad8d4ed358
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 11:32:31 +0530

    Explained when Mounting and Update happens

--- Class-1/my-app/src/Components/UseEffect.js
+++ Class-1/my-app/src/Components/UseEffect.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+
 let UseEffect = function(){
+    let [count,setCount] = useState(0);
+    
+    let valInc = function(){
+        setCount(count+1);
+    }
+
+    useEffect(function cb1(){
+        console.log("This is to be printed in the Mounting Phase");
+    },[]);
+
+    // * cb1 -> will be executed when the component renders for the first time in the UI.
+
+    useEffect(function cb2(){
+        console.log("This is to be printed in the Mounting and Update Phase");
+    },[count]);
+
+    // * cb2 -> will be executed when the component renders for the first time in the UI and when STATE Changes.
+
     return(
-        <div>This is the Use Effect Hook : 0</div>
+        <div>The value of count is : {count}
+        <button onClick={valInc}>Increment</button>
+        </div>
+        
     )
 }
 
