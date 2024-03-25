81b83b93f7f9498e447fec7f76ae51c44f7b6703
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 20:28:31 +0530

    Implemented dynamic title for all the movie card

--- Class-2/tmdb-website/src/Components/Movie.js
+++ Class-2/tmdb-website/src/Components/Movie.js
@@ -3,7 +3,7 @@ function Movie(props){
             <div className="h-[50vh] w-[200px] bg-center bg-cover rounded-md m-6 hover:scale-105 duration-300 cursor-pointer flex items-end overflow-hidden"
             style={{backgroundImage:`url(https://m.media-amazon.com/images/M/MV5BYjQxMWY5NjgtNDI2NS00MWM2LTg2MDAtNjI5NGIwZTNmYWZiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg)`}}>
                 <div className="w-full text-center bg-neutral-900/60 text-white p-2 text-xl">
-                    Migration
+                    {props.title}
                 </div>
             </div>
     )
--- Class-2/tmdb-website/src/Components/Movielist.js
+++ Class-2/tmdb-website/src/Components/Movielist.js
@@ -1,13 +1,30 @@
 import Movie from "./Movie";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Movielist(){
+
+    let [movies,setMovies] = useState([]);
+
+    useEffect(function(){
+        axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=684a5e1c0a061b97c5014def1d3f05d6')
+        .then(function(res){
+            // console.log(res.data.results);
+            setMovies(res.data.results);
+        })
+    },[]);
+    
     return (
         <div>
             <h3 className="text-3xl font-bold text-center p-[3rem] text-cyan-800">Trending Movies</h3>
 
             <div className="flex flex-wrap justify-around">
             
-                <Movie/>
+                {
+                    movies.map(function(ele){
+                        return <Movie title={ele.title}/>
+                    })
+                }
 
             </div>
 
