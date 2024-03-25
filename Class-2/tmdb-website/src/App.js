1a25699145ac210639f0b17238924e5251707260
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 17:46:21 +0530

    On the WatchList Route rendered the WatchList Component

--- Class-2/tmdb-website/src/App.js
+++ Class-2/tmdb-website/src/App.js
@@ -4,13 +4,16 @@ import Banner from './Components/Banner';
 import Movie from './Components/Movie';
 import Pagination from './Components/Pagination';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Watchlist from './Components/Watchlist';
 
 function App() {
   return (
     <>
 		<BrowserRouter>
 		<Navbar />
+
 		<Routes>
+			{/* This is the Home Page Route */}
 			<Route path="/" element={
 				<>
 					<Banner/>
@@ -18,7 +21,17 @@ function App() {
 				</>
 			}>
 			</Route>
+			
+			{/* This is the Watchlist Route */}
+			<Route path="/watchlist" element={
+				<>
+					<Watchlist />
+				</>
+			}>
+
+			</Route>
       	</Routes>
+
   		</BrowserRouter>
 		{/* <Navbar/>
 		<Banner />
