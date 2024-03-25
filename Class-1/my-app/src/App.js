c42b06044619ea39849fc8823c39a1bdb1175b31
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 11:00:02 +0530

    Introduction to Use Effect Hook

--- Class-1/my-app/src/App.js
+++ Class-1/my-app/src/App.js
@@ -5,12 +5,14 @@ import UserProfile from './Components/UserProfile';
 import Counter from './Components/Counter';
 import Products from './Components/Products';
 import Forms from './Components/Forms';
+import UseEffect from './Components/UseEffect';
 
 function App() {
   return (
 	<>
+		<UseEffect/>
 		{/* <Header/> */}
-		<Forms/>
+		{/* <Forms/> */}
 		{/* <Header/> */}
 		{/* <UserProfile name="Subhadip" age="29" place="India"/> */}
 		{/* <UserProfile name="Nikhil" age="29" place = "Achen"/> */}
---
+++ Class-1/my-app/src/Components/UseEffect.js
@@ -0,0 +1,7 @@
+let UseEffect = function(){
+    return(
+        <div>This is the Use Effect Hook : 0</div>
+    )
+}
+
+export default UseEffect;
