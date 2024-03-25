712fc227f93c4375c0d1e6a8e58b3c467c586f92
Subhadip Das <subhadipdas911@gmail.com>
2024-03-25 12:06:27 +0530

    Cb2 will be executed only when the count state is getting updated

--- Class-1/my-app/src/Components/UseEffect.js
+++ Class-1/my-app/src/Components/UseEffect.js
@@ -18,16 +18,16 @@ let UseEffect = function(){
 
     // * cb1 -> will be executed when the component renders for the first time in the UI.
 
-    // useEffect(function cb2(){
-    //     console.log("This is to be printed in the Mounting and Update Phase");
-    // },[count]);
+    useEffect(function cb2(){
+        console.log("This is to be printed in the Mounting and Update Phase");
+    },[count]);
 
     // * cb2 -> will be executed when the component renders for the first time in the UI and when STATE Changes.
 
 
-    useEffect(function cb3(){
-        console.log("This will also run in the Mounting and Updating Phase");
-    })
+    // useEffect(function cb3(){
+    //     console.log("This will also run in the Mounting and Updating Phase");
+    // })
 
     // * cb3 -> will be executed in the Mounting and Updating Phase
 
