9e70976ff4aaade20d5244cae2071c23c20128d4
Subhadip Das <subhadipdas911@gmail.com>
2024-03-26 22:51:16 +0530

    Creation of Dynamic Todo List Complete

--- TodoApp/todo-app/src/Components/Todolist.js
+++ TodoApp/todo-app/src/Components/Todolist.js
@@ -1,12 +1,25 @@
 import TodoItem from "./TodoItem";
 import './../TodoList.css';
+import { useState } from "react";
 
 function TodoList(){
 
-    let todoListArr = [{id:237,todoData:"Todo Item1"},{id:256,todoData:"Todo Item2"},{id:272,todoData:"Todo Item3"}];
+    // let todoListArr = [{id:237,todoData:"Todo Item1"},{id:256,todoData:"Todo Item2"},{id:272,todoData:"Todo Item3"}];
+
+    let [todoListArr,setTodoListArr] = useState([])
+    let [getTodo,setTodo] = useState("");
+
+    function addTodo(){
+        setTodoListArr([...todoListArr,{id:(new Date()).getTime(),todoData:getTodo}]);
+        setTodo("")
+    }
 
     return(
         <div className="list-todo">
+            <div className="top-header">
+                <input value={getTodo} onChange={(e) => setTodo(e.target.value)}/>
+                <button onClick={addTodo}>Add Todo</button>
+            </div>
             {
                 todoListArr.map(function(res){
                     return <TodoItem key={res.id} todoDetail={res.todoData} todoID={res.id}/>
