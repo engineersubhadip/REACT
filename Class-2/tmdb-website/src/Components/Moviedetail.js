630adf3d78a97b1ff4ab902a7b59869ca62314dd
Subhadip Das <subhadipdas911@gmail.com>
2024-03-26 13:28:54 +0530

    Implemented the Movie Detail Page

--- Class-2/tmdb-website/src/Components/Moviedetail.js
+++ Class-2/tmdb-website/src/Components/Moviedetail.js
@@ -1,24 +1,37 @@
 import {useState,useEffect} from 'react';
+import axios from 'axios';
 
 function MovieDetail(props){
-    let [movieID,setMovieID] = useState(null);
+    let [movieResponse,updateResponse] = useState({});
     
     useEffect(function(){
         let currentURL = window.location.href;
         let updatedURL = currentURL.split('/');
         let updatedID = updatedURL[updatedURL.length-1];
-        setTimeout(function(){
-            setMovieID(updatedID);
-        },5000);
+        axios.get(`https://api.themoviedb.org/3/movie/${updatedID}?api_key=684a5e1c0a061b97c5014def1d3f05d6`)
+        .then(function(res){
+            
+            let title = res.data.title;
+            let img = `https://image.tmdb.org/t/p/original/${res.data.backdrop_path}`;
+            let descp = res.data.overview;
+            let rating = Math.round(res.data.vote_average,2);
+            
+            updateResponse({title:title,image:img,description:descp,rating:rating});
+        })
     },[]);
 
 
-    if(movieID===null){
-        return <div>Loading...</div>
-    }
-
     return(
-        <div>This is the Movie Detail Component</div>
+        <div className='flex justify-center gap-20 p-[2rem]'>
+            <div className='h-[60vh] rounded-lg  min-w-96 bg-center bg-cover' style={{backgroundImage:`url(${movieResponse.image})`}}></div>
+
+            <div className='flex flex-col justify-between'>
+            <div className='text-2xl font-bold text-cyan-800'>{movieResponse.title}</div>
+            <div className='text-cyan-800'>{movieResponse.description}</div>
+            <div className='text-xl font-bold text-cyan-800'>Rating : {movieResponse.rating}/10</div>
+            </div>
+
+        </div>
     )
 }
 
